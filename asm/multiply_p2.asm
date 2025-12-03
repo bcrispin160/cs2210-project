3eb282b9e99576aabaf1816fc1f041491272b713
gsalbr <gretasalbrecht@gmail.com>
2025-12-03 10:59:41 -0500

    completed multiply_p2.asm

--- asm/multiply_p2.asm
+++ asm/multiply_p2.asm
@@ -1,11 +1,20 @@
 ; Multiplies some constant by powers of two and leaves the result in registers
 
-LOADI R0, #0x03        ; load constant 3 into R0
-LOADI R1, #0x06        ; load constant 6 into R1 (greatest power of two we multiply by)
-SHFT  R2, R0, #0x01    ; shift R0 left by 1 (*2^1) and store result in R2
-SHFT  R3, R0, #0x02    ; shift R0 left by 2 (*2^2) and store result in R3
-SHFT  R4, R0, #0x03    ; shift R0 left by 3 (*2^3) and store result in R4
-SHFT  R5, R0, #0x04    ; shift R0 left by 4 (*2^4) and store result in R5
-SHFT  R6, R0, #0x05    ; shift R0 left by 5 (*2^5) and store result in R6
-SHFT  R7, R0, #0x06    ; shift R0 left by 6 (*2^6) and store result in R7
+START:
+    LOADI R0, #0x03        ; load constant 3 into R0
+    LOADI R1, #0x01        ; load constant 1 into R1 (first power of two we multiply by)
+    SHFT  R2, R0, R1       ; shift R0 left by R1 (*2^R1) and store in R2
+    ADDI R1, R1, #0x01     ; increment R1 by 1
+    SHFT  R3, R0, R1       ; shift R0 left by R1 (*2^R1) and store in R3
+    ADDI R1, R1, #0x01     ; increment R1 by 1
+    SHFT  R4, R0, R1       ; shift R0 left by R1 (*2^R1) and store in R4
+    ADDI R1, R1, #0x01     ; increment R1 by 1
+    SHFT  R2, R0, R5       ; shift R0 left by R1 (*2^R1) and store in R5
+    ADDI R1, R1, #0x01     ; increment R1 by 1
+    SHFT  R2, R0, R6       ; shift R0 left by R1 (*2^R1) and store in R6
+    ADDI R1, R1, #0x01     ; increment R1 by 1
+    SHFT  R2, R0, R7       ; shift R0 left by R1 (*2^R1) and store in R7
+
+END:
+    HALT
 
