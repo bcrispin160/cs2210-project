343566fc94452898bd439502108327dc9b580b82
bcrispin160 <brynlee.crispin@uvm.edu>
2025-12-05 18:20:21 -0500

    Update divide_p2.asm

--- asm/divide_p2.asm
+++ asm/divide_p2.asm
@@ -1,9 +1,9 @@
 START:
     LUI     R0, 0x01
-    ADDI    R0, R0, 0xC0
+    LOADI   R0, 0xC0
 
     LUI     R1, 0x80
-    ADDI    R1, R1, 0x06
+    LOADI   R1, 0x06
 
     SHFT    R2, R0, R1
     ADDI    R1, R1, 1
