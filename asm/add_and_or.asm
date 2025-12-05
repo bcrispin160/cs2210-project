f3068312554c6e4e56be4bc846fdfa1cd6bb47c0
bcrispin160 <brynlee.crispin@uvm.edu>
2025-12-05 18:41:10 -0500

    Update add_and_or.asm

--- asm/add_and_or.asm
+++ asm/add_and_or.asm
@@ -2,15 +2,18 @@ START:
     LOADI   R0, OxAA
     LOADI	R1, Ox55
     
-    AND    R3, R0, R1
+    AND R3, R0, R1
     
-    LOADI    R4, 1
-    SUB     R4, R4, R3
-    SHFT     R4, R4, 8
-    SHFT     R4, R4, -8
+    BNE R3, UNSAFE
+    
+    
+SAFE:
+    OR R2, R0, R1
+    
 
-    OR     R2, R0, R1
-    AND    R2, R2, R4
+UNSAFE:
+    LOADI R2, 0
 
-HALT
+DONE:
+    HALT
 
