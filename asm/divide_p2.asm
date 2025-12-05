9200277f6744108a9d5f60486444913591f2cedc
bcrispin160 <brynlee.crispin@uvm.edu>
2025-12-05 17:35:27 -0500

    Update divide_p2.asm

--- asm/divide_p2.asm
+++ asm/divide_p2.asm
@@ -2,10 +2,16 @@ START:
     LOADI   R0, 448        ; original constant
     LOADI   R1      ;last operand used by SHFT
     SHFT	R2, R0, R1	;448 / 2^1
+
+    LOADI   R2
     SHFT	R3, R0, R1	;448 / 2^2
+    LOADI   R3
     SHFT	R4, R0, R1	;448 / 2^3
+    LOADI   R4
     SHFT	R5, R0, R1	;448 / 2^4
+    LOADI   R5
     SHFT	R6, R0, R1	;448 / 2^5
+    LOADI   R6
     SHFT	R7, R0, R1	;448 / 2^6
 DONE:
     HALT
