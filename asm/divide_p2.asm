9a89b0c27c7ebe664a8055700318c4e8e70c99f7
bcrispin160 <brynlee.crispin@uvm.edu>
2025-12-05 17:42:09 -0500

    Update divide_p2.asm

--- asm/divide_p2.asm
+++ asm/divide_p2.asm
@@ -3,15 +3,15 @@ START:
     LOADI   R1      ;last operand used by SHFT
     SHFT	R2, R0, R1	;448 / 2^1
 
-    LOADI   R2
+    LOADI   R1, 2
     SHFT	R3, R0, R1	;448 / 2^2
-    LOADI   R3
+    LOADI   R1, 3
     SHFT	R4, R0, R1	;448 / 2^3
-    LOADI   R4
+    LOADI   R1, 4
     SHFT	R5, R0, R1	;448 / 2^4
-    LOADI   R5
+    LOADI   R1, 5
     SHFT	R6, R0, R1	;448 / 2^5
-    LOADI   R6
+    LOADI   R1, 6
     SHFT	R7, R0, R1	;448 / 2^6
 DONE:
     HALT
